f7d5a71b5856aa23d0df8051162cde1d507ed00b
sanjuro-dev <anpgigo@gmail.com>
2026-05-27 20:35:17 -0300

    Adding data exploration analysis

--- query.sql
+++ query.sql
@@ -1,7 +1,20 @@
+-- MAIORES LAYOFFS POR EMPRESA E ANO
+
+select company, year(`date`) as year, sum(total_laid_off) as total
+from layoff_staging2 group by company,year( `date`)
+having total is not null order by total;
 
 
--- TRATAR DADOS NULOS OU VAZIOS
--- REMOVER
+
+WITH company_year (company, years, total_laid_off) as
+(
+    select company, year(`date`) as year, sum(total_laid_off) as total
+    from layoff_staging2 group by company,year( `date`)
+    having total is not null and year is not null
+    ) select *, dense_rank() over (partition by years order by total_laid_off desc) as ranking
+    from company_year order by ranking;
+
+
 
 
 CREATE SCHEMA layoff;
@@ -69,6 +82,8 @@ ALTER TABLE layoff_staging2
 
 select date from layoff_staging2;
 
+-- TRATAR DADOS NULOS OU VAZIOS
+
 select * from layoff_staging2 where industry = 'NULL' or '';
 update layoff_staging2 set industry = null where industry = 'NULL' or industry = '';
 
@@ -110,3 +125,77 @@ delete from layoff_staging2
 where total_laid_off is null and percentage_laid_off is null;
 
 alter table layoff_staging2 drop row_num;
+
+
+-- ANALISE DOS DADOS TRANSFORMADOS
+
+select * from layoff_staging2;
+
+-- PERIODO DE ANALISE
+select min(`date`) as start, max(`date`) as end from layoff_staging2;
+
+-- MAXIMO DE LAYOFFS
+select max(total_laid_off) as absolute, max(percentage_laid_off) as relative
+from layoff_staging2;
+
+-- EMPRESAS QUE FORAM A FALENCIA
+
+select * from layoff_staging2 where percentage_laid_off=1
+order by funds_raised_millions desc;
+
+-- LAYOFFS TOTAIS POR EMPRESA
+
+select company, sum(total_laid_off) as total
+from layoff_staging2
+group by company order by 2 desc;
+
+-- LAYOFFS TOTAIS POR SETOR
+
+select industry, sum(total_laid_off) as total
+from layoff_staging2
+group by industry order by 2 desc;
+
+-- LAYOFFS TOTAIS POR PAIS
+
+select country, sum(total_laid_off) as total
+from layoff_staging2
+group by country order by 2 desc;
+
+-- LAYOFFS TOTAIS POR ANO
+select year(`date`), sum(total_laid_off) as total
+from layoff_staging2
+group by year(`date`) order by 2 desc;
+
+-- LAYOFFS TOTAIS POR MES
+
+select substring(`date`, 6,2) as 'month', year(`date`) as 'year', sum(total_laid_off) as total
+from layoff_staging2 where substring(`date`, 6,2) is not null and year(`date`) is not null
+GROUP BY month ,year
+order by year, month;
+
+-- TOTAL ACUMULADO
+
+with rolling_total as (
+    select substring(`date`, 6,2) as `month`, year(`date`) as `year`, sum(total_laid_off) as total
+    from layoff_staging2 where substring(`date`, 6,2) is not null and year(`date`) is not null
+    GROUP BY month ,year
+    order by year, month
+)
+
+select month,total, sum(total) over(order by month, year) as rolling_total from rolling_total;
+
+-- MAIORES LAYOFFS POR EMPRESA E ANO
+
+select company, year(`date`) as year, sum(total_laid_off) as total
+from layoff_staging2 group by company,year( `date`)
+having total is not null order by total;
+
+-- RANKING
+
+WITH company_year (company, years, total_laid_off) as
+(
+    select company, year(`date`) as year, sum(total_laid_off) as total
+    from layoff_staging2 group by company,year( `date`)
+    having total is not null and year is not null
+    ) select *, dense_rank() over (partition by years order by total_laid_off desc) as ranking
+    from company_year order by ranking;
